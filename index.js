ca14fcef07ad07daec7c2641fb5b911e0f9a986c
Zoya Tanweer <87615284+zoyatanweer@users.noreply.github.com>
2021-09-27 20:42:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ var userName = readlineSync.question("What's your name?");
 console.log("Hi " + userName + " . Let's begin with the The Office quiz!");
 var score = 0;
 
+// high score of people
+var highScores = [
+  {
+    name: "Mayank",
+    score: 4,
+  },
+  {
+    name: "Priyal",
+    score: 3,
+  }
+]
+
 //function
 function play(question, answer){
   var userAnswer = readlineSync.question(question);
@@ -41,10 +53,20 @@ var questions =[
 }
 ];
 
-for(var i=0;i<questions.length;i++){
-  var currentQuestion = questions[i];
-  play(currentQuestion.question, currentQuestion.answer);
+function game(){
+  for(var i=0;i<questions.length;i++){
+     var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer);
+  }
 }
 
 //final score display
-console.log("You completed the quiz! Your final score is " + score);
+function showScores(){
+  console.log("You completed the quiz! Your final score is " + score);
+  console.log("If you have beaten the high score, tell me and I will update it")
+
+  highScores.map(score => console.log(score.name, " : ", score.score))
+}
+
+game();
+showScores();
